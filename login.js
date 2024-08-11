cfada54b331c94963032c067f92eb5146b27d940
Anvesh Gandham <anvesh.g10@gmail.com>
2024-08-11 19:56:47 +0530

    added login check functionality and added changes for the display

--- login.js
+++ login.js
@@ -1,15 +1,32 @@
 function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    // Store credentials in local storage
-    const storedUsername = localStorage.getItem('username') || 'admin';
-    const storedPassword = localStorage.getItem('password') || 'password';
+  // Store credentials in local storage
+  const storedUsername = localStorage.getItem("username") || "admin";
+  const storedPassword = localStorage.getItem("password") || "password";
 
-    if (username === storedUsername && password === storedPassword) {
-        // Redirect to resume page
-        window.location.href = 'resume.html';
-    } else {
-        document.getElementById('error-message').innerText = 'Invalid username/password';
-    }
+  if (username === storedUsername && password === storedPassword) {
+    // Redirect to resume page
+    window.location.href = "resume.html";
+    sessionStorage.setItem("loginResume", true);
+  } else {
+    document.getElementById("error-message").innerText =
+      "Invalid username/password";
+  }
 }
+
+const isLoggedIn = sessionStorage.getItem("loginResume");
+
+if (isLoggedIn === "true") {
+  window.location.href = "resume.html";
+}
+
+window.onpageshow = function (event) {
+  if (event.persisted) {
+    // This block is triggered when the page is loaded from the browser cache
+    if (sessionStorage.getItem("loginResume") === "true") {
+      window.location.href = "login.html";
+    }
+  }
+};
--- resume.js
+++ resume.js
@@ -2,78 +2,102 @@ let applicants = [];
 let filteredApplicants = [];
 let currentIndex = 0;
 
-fetch('./Data.json')
-    .then(response => response.json())
-    .then(data => {
-        applicants = data.resume;  
-        console.log("resumes data:", applicants);
-        displayApplicant(applicants, currentIndex);
-    });
+fetch("./Data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    applicants = data.resume;
+    console.log("resumes data:", applicants);
+    displayApplicant(applicants, currentIndex);
+  });
 
 function displayApplicant(applicants, index) {
-    if (applicants.length > 0) {
-        const applicant = applicants[index];
-       document.getElementById('applicant-details').innerHTML = makeHtmlForApplicant(applicant); 
+  if (applicants.length > 0) {
+    const applicant = applicants[index];
+    document.getElementById("applicant-details").innerHTML =
+      makeHtmlForApplicant(applicant);
 
-        // Hide buttons as necessary
-        document.getElementById('prevBtn').style.visibility = index === 0 ? 'hidden' : 'visible';
-        document.getElementById('nextBtn').style.visibility = index === applicants.length - 1 ? 'hidden' : 'visible';
-    } else {
-        document.getElementById('error-message').innerText = 'No applicants found';
-    }
+    // Hide buttons as necessary
+    document.getElementById("prevBtn").style.visibility =
+      index === 0 ? "hidden" : "visible";
+    document.getElementById("nextBtn").style.visibility =
+      index === applicants.length - 1 ? "hidden" : "visible";
+  } else {
+    document.getElementById("error-message").innerText = "No applicants found";
+  }
 }
 
 function nextApplicant() {
-    const  resumes = (filteredApplicants.length > 0) ? filteredApplicants : applicants;
-    if (currentIndex < resumes.length - 1) {
-        currentIndex++;
-        displayApplicant(resumes, currentIndex);
-    }
+  const resumes =
+    filteredApplicants.length > 0 ? filteredApplicants : applicants;
+  if (currentIndex < resumes.length - 1) {
+    currentIndex++;
+    displayApplicant(resumes, currentIndex);
+  }
 }
 
 function prevApplicant() {
-    const  resumes = (filteredApplicants.length > 0) ? filteredApplicants : applicants;
-    if (currentIndex > 0) {
-        currentIndex--;
-        displayApplicant(resumes, currentIndex);
-    }
+  const resumes =
+    filteredApplicants.length > 0 ? filteredApplicants : applicants;
+  if (currentIndex > 0) {
+    currentIndex--;
+    displayApplicant(resumes, currentIndex);
+  }
 }
 
 function debounce(func, delay) {
-    let timeoutId;
-    return function(...args) {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
 }
 
 function filterByJob() {
-    const filter = document.getElementById('jobFilter').value.toLowerCase();
-    filteredApplicants = applicants.filter(applicant => applicant.basics.AppliedFor.toLowerCase().includes(filter));
+  const filter = document.getElementById("jobFilter").value.toLowerCase();
+  filteredApplicants = applicants.filter((applicant) =>
+    applicant.basics.AppliedFor.toLowerCase().includes(filter)
+  );
 
-    if (filteredApplicants.length > 0) {
-        currentIndex = 0;
-        displayApplicant(filteredApplicants, currentIndex);
-        document.querySelector('.error-message-div').style.display = 'none';
-        document.getElementById('applicant-details').style.display = "block";
-    } else {
-        document.querySelector('.error-message-div').style.display = 'flex';
-        document.getElementById('applicant-details').style.display = "none";
-    }
+  if (filteredApplicants.length > 0) {
+    currentIndex = 0;
+    displayApplicant(filteredApplicants, currentIndex);
+    document.querySelector(".error-message-div").style.display = "none";
+    document.getElementById("applicant-details").style.display = "block";
+  } else {
+    document.querySelector(".error-message-div").style.display = "flex";
+    document.getElementById("applicant-details").style.display = "none";
+  }
 }
 
-const inputElement = document.getElementById('jobFilter');
-inputElement.addEventListener('keyup', debounce(filterByJob, 300));
+const inputElement = document.getElementById("jobFilter");
+inputElement.addEventListener("keyup", debounce(filterByJob, 300));
 
-function makeHtmlForApplicant (applicant) {
+function makeHtmlForApplicant(applicant) {
+  let skills = applicant.skills.keywords
+    .map((skill) => `<p>${skill}</p>`)
+    .join("");
+  let hobbies = applicant.interests.hobbies
+    .map((hobby) => `<p>${hobby}</p>`)
+    .join("");
+  const allEductationDetails = applicant.education;
 
-    let skills = applicant.skills.keywords.map((skill) => `<p>${skill}</p>`).join('');
-    console.log("skills:", skills);
-    let hobbies = applicant.interests.hobbies.map((hobby) => `<p>${hobby}</p>`).join('');
-    return (
-        `<div class="applicant-header">
+  let eductationDetails = [];
+
+  for (let degree in allEductationDetails) {
+    eductationDetails.push(`<li>
+        <p><b>${degree}</b>: ${allEductationDetails[degree]["institute"]}, ${
+      allEductationDetails[degree]?.["course"] ?? ""
+    }  ${allEductationDetails[degree]?.["Start Date"] ?? ""} ${
+      allEductationDetails[degree]?.["End Date"] ?? ""
+    } ${allEductationDetails[degree]["cgpa"]}</p>
+        </li>`);
+  }
+
+  eductationDetails = eductationDetails.join("");
+
+  return `<div class="applicant-header">
                 <div class="name-div">
                     <h3>${applicant.basics.name}</h3>
                     <p>Applied For ${applicant.basics.AppliedFor} </p>
@@ -96,39 +120,42 @@ function makeHtmlForApplicant (applicant) {
                 <div class="professional-info-div">
                     <h3>Work Experience in previous company</h3>
                     <div class="explanation-div">
-                        <p><b>Company Name:</b> </p>
-                        <p><b>Position:</b> </p>
-                        <p><b>Start Date:</b> </p>
-                        <p><b>End Date:</b> </p>  
-                        <p><b>Summary:</b> </p>   
+                        <p><b>Company Name:</b> ${applicant.work["Company Name"]}</p>
+                        <p><b>Position:</b> ${applicant.work.Position}</p>
+                        <p><b>Start Date:</b> ${applicant.work["Start Date"]}</p>
+                        <p><b>End Date:</b> ${applicant.work["End Date"]}</p>  
+                        <p><b>Summary:</b> ${applicant.work.Summary}</p>   
                     </div>
                     <h3>Projects</h3>
                     <div class="explanation-div">
-                        <p><b>Company Name:</b> </p>
+                        <p><b>${applicant.projects.name}:</b> ${applicant.projects.description} </p>
                     </div>
                     <h3>Education</h3>
                     <div class="explanation-div">
                         <ul>
-                            <li> <p><b>Company Name:</b> </p></li>
+                            ${eductationDetails}
                         </ul>
                     </div>
                     <h3>Internship</h3>
                     <div class="explanation-div">
-                        <p><b>Company Name:</b> </p>
-                        <p><b>Position:</b> </p>
-                        <p><b>Start Date:</b> </p>
-                        <p><b>End Date:</b> </p>  
-                        <p><b>Summary:</b> </p>   
+                         <p><b>Company Name:</b> ${applicant.Internship["Company Name"]}</p>
+                        <p><b>Position:</b> ${applicant.Internship.Position}</p>
+                        <p><b>Start Date:</b> ${applicant.Internship["Start Date"]}</p>
+                        <p><b>End Date:</b> ${applicant.Internship["End Date"]}</p>  
+                        <p><b>Summary:</b> ${applicant.Internship.Summary}</p>     
                     </div>
                     <h3>Achievements</h3>
                     <div class="explanation-div">
                         <ul>
-                            <li> <p>Summary </p></li>
+                            <li> <p>${applicant.achievements.Summary} </p></li>
                         </ul> 
                     </div>
                 </div>
 
-            </div>`
-    );
+            </div>`;
+}
 
+// Restrict access if not logged in
+if (sessionStorage.getItem("loginResume") !== "true") {
+  window.location.href = "index.html";
 }
